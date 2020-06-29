7e1f68f70ce5c58852614866d5de84e6755bac43
Ginelle Plaisir <plaisirginelle@gmail.com>
2020-06-29 04:56:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
+<<<<<<< HEAD
   'SELECT species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(*) DESC LIMIT 1'
+=======
+  'SELECT '
+>>>>>>> 7b394f54da2c86dc354b0c1e7bca5f37f44a00a0
 end
 
 def select_name_and_series_subgenres_of_authors
@@ -16,7 +20,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
+<<<<<<< HEAD
   "SELECT series.title FROM series INNER JOIN characters ON series.author_id = characters.author_id WHERE characters.species = 'human' GROUP BY title ORDER BY COUNT(*) DESC LIMIT 1"
+=======
+ 
+>>>>>>> 7b394f54da2c86dc354b0c1e7bca5f37f44a00a0
 end
 
 def select_character_names_and_number_of_books_they_are_in
